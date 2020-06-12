462ef55e409b89b9ea55ee28e56843db030d11dc
TheDragonHill <46721121+TheDragonHill@users.noreply.github.com>
2020-06-12 20:18:01 +0200

    Credits in Scene

---
+++ Assets/Scripts/C#/Individuals/StartCredits.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartCredits : Interactable
+{
+	public Animator transition;
+
+	public override void Interact()
+	{
+		StartCoroutine(LoadLevel());
+	}
+	
+	IEnumerator LoadLevel()
+	{
+		//Play animation
+		transition.SetTrigger("Start");
+
+		//Wait
+		yield return new WaitForSeconds(4);
+
+		//Load scene
+		SceneManager.LoadScene("Credits");
+	}
+}
