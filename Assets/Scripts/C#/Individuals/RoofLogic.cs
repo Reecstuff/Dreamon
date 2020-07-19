9092f996e8d3adae0d6debc031e05ab986293ab4
Christian <hc.dolling@mail.de>
2020-07-19 20:06:48 +0200

    Added RoofLogic

---
+++ Assets/Scripts/C#/Individuals/RoofLogic.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+
+/// <summary>
+/// Switch roof between states
+/// </summary>
+[RequireComponent(typeof(Collider))]
+public class RoofLogic : MonoBehaviour
+{
+    [SerializeField]
+    GameObject roofToDeactivate;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check Player
+        if(other.GetComponent<PlayerController>())
+            SwitchRoofView(false);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //Check Player
+        if (other.GetComponent<PlayerController>())
+            SwitchRoofView(true);
+    }
+
+
+    /// <summary>
+    /// Switch Roof
+    /// </summary>
+    /// <param name="isShown">Does Roof exist or not</param>
+    void SwitchRoofView(bool isShown)
+    {
+        if(roofToDeactivate)
+        {
+            roofToDeactivate.SetActive(isShown);
+        }
+    }
+}
